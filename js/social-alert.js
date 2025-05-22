5015d5811b2792cf2fbd27b5a7b1a78ae4d6aff9
Ramiruuu <oclaritremar25@gmail.com>
2025-05-22 23:02:09 +0800

    Added Social Alerts

--- js/social-alert.js
+++ js/social-alert.js
@@ -3,8 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
         link.addEventListener('click', function(e) {
             e.preventDefault();
             const url = this.href;
-            alert('You are about to visit my social media profile!');
-            window.open(url, '_blank');
+            Swal.fire({
+                title: 'You are leaving ByteMark',
+                text: 'You are about to visit my social media profile. Do you want to continue?',
+                icon: 'info',
+                iconColor: '#5368df',
+                confirmButtonText: 'Yes, open in new tab',
+                confirmButtonColor: '#5368df',
+                showCancelButton: true,
+                cancelButtonText: 'Cancel',
+                background: '#f7f8fa',
+                color: '#3246a8',
+                customClass: {
+                    popup: 'swal2-border-radius'
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.open(url, '_blank');
+                }
+            });
         });
     });
 });
